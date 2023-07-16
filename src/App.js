9e8b5be71f75f0f8a5681d3b9e2b74f83de15777
Nurkulova <cholponnurkulova@gmail.com>
2023-07-17 01:45:40 +0300

    imported CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Expenses from './components/expenses/Expenses'
 import NewExpense from './components/new-expense/NewExpense'
+import './App.css'
 
 const EXPENSES = [
 	{
